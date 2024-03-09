056c1ae373a5b0019c79d25ccabd4eebdf734ded
nidhirudani <Nidhi.Rudani@bbd.co.za>
2024-03-09 11:54:06 +0530

    Initial commit

---
+++ src/test/java/com/saveIt/saveIt/SaveItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveIt.saveIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
